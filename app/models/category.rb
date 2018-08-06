3aaefc4b6ffac501539f738c9fe3d43a1cf39f2e
moorec104 <moorec104@gmail.com>
2018-08-05 18:27:05 -0700

    added next_pos

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,9 @@ class Category < ApplicationRecord
   def ordered_phrases
     phrase_categories.sort_by { |pc| pc.position }.map { |pc| pc.phrase }
   end
+
+  def next_pos
+    ordered_phrases.size
+  end
 end
 
