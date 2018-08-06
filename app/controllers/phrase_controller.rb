9b58eff9cacc646c4a69689fa1e0199f1a693a30
moorec104 <moorec104@gmail.com>
2018-08-05 18:25:43 -0700

    fixed append

--- app/controllers/phrase_controller.rb
+++ app/controllers/phrase_controller.rb
@@ -1,9 +1,4 @@
 class PhraseController < ApplicationController
-  def index
-    @categories = Category.all
-    @phrases_without_category = Phrase.where(category: nil)
-  end
-
   def create
     category_id = params[:category_id]
     category = category_id ? Category.find(category_id) : nil
@@ -20,7 +15,13 @@ class PhraseController < ApplicationController
     phrase = Phrase.find(params[:phrase_id])
     case params[:type]
     when 'CATEGORY'
-      phrase.update!(category: Category.find(params[:category_id]))
+      category = Category.find(params[:category_id])
+      phrase_category = PhraseCategory.new(
+        phrase_id: phrase.id,
+        category_id: category.id,
+        position: category.next_pos
+      )
+      category.phrase_categories << phrase_category 
     end
   end
 end
