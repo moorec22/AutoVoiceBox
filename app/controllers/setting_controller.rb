7e86be57350faf536683cf3a7ed873202826c813
Connor Moore <connor.moore@airbnb.com>
2019-12-30 13:00:23 -0800

    added a setting for fixed category

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -44,5 +44,13 @@ class SettingController < ApplicationController
     setting.update(value: category)
     render partial: "category/base", locals: { category: Category.find(setting.value) }
   end
+
+  def get_fixed_category_id
+    setting = Setting.where(name: 'fixed_category').first
+    if !setting || !Category.where(id: setting.value).first
+      setting = Setting.create!(name: 'current_category', value: (Category.first.try(:id) || 0))
+    end
+    setting.value.to_i
+  end
 end
 
