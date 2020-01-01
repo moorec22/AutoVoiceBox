33d87a21d74f195e80a4c29268bb8c18a5fa60d1
Connor Moore <connor.moore@airbnb.com>
2019-12-31 16:12:32 -0800

    fixed deleting

--- app/controllers/phrase_controller.rb
+++ app/controllers/phrase_controller.rb
@@ -8,7 +8,8 @@ class PhraseController < ApplicationController
   def destroy
     PhraseCategory.where(phrase_id: params[:phrase_id]).each { |pc| pc.destroy! }
     phrase = Phrase.find(params[:phrase_id])
-    phrase.destroy!
+    phrase.destroy
+  rescue ActiveRecord::RecordNotFound
   end
 
   def update
